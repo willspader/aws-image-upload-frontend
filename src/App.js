144f8cac3f7f23d4142e072a2ac5becccf4767f0
willspader <spaderwilliam@hotmail.com>
2020-07-15 14:53:14 -0300

    download image

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ const UserProfiles = () => {
   return userProfiles.map((userProfile, index) => {
     return (
       <div key={index}>
+        {userProfile.userProfileId ? <img src={`http://localhost:8080/api/v1/user-profile/${userProfile.userProfileId}/image/download`}/> : null}
         {/* todo: profile image */}
         <br />
         <br />
